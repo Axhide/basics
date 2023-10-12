8a1cb8855dd230f79e8e4aad5d704f0b5da06709
Axhide <borovoy4312@gmail.com>
2023-10-12 15:38:07 +0300

    Props and gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {StyledBtn} from "./components/Button.styled";
+import {Box} from "./components/Box.styled";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Box>
+                {/*<StyledBtn color="green" fontSize={"20px"}>Hello</StyledBtn>*/}
+                {/*<StyledBtn color="red">Hello</StyledBtn>*/}
+                {/*<StyledBtn fontSize={"30px"}>Hello</StyledBtn>*/}
+
+                <StyledBtn btnType={"primary"} active>Hello</StyledBtn>
+                <StyledBtn btnType={"outline"}>Hello</StyledBtn>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
